3023d4e88fcf5edb3d363e193dc5b7910ddf7a3d
Zsolt Szende <zsolt@artificial.io>
2020-12-02 23:32:17 +0000

    Day 02

--- src/AoC.hs
+++ src/AoC.hs
@@ -1,11 +1,13 @@
 module AoC (solutions) where
 
 import AoC.Days.Day01 qualified as Day01
+import AoC.Days.Day02 qualified as Day02
 import AoC.Lib.Day
 import AoC.Lib.Solver
 import AoC.Prelude
 
 solutions :: [Day -> IO ()]
 solutions =
-  [ mkSolver Day01.parse Day01.solveA Day01.solveB
+  [ mkSolver Day01.parse Day01.solveA Day01.solveB,
+    mkSolver Day02.parse Day02.solveA Day02.solveB
   ]
---
+++ src/AoC/Days/Day02.hs
@@ -0,0 +1,41 @@
+module AoC.Days.Day02 where
+
+import AoC.Lib.Parser
+import AoC.Prelude
+import Text.Megaparsec.Char.Lexer qualified as L
+
+parse :: String -> Maybe [Row]
+parse = parseFileWith passP
+
+solveA :: [Row] -> Int
+solveA = length . filter isValidRowA
+
+solveB :: [Row] -> Int
+solveB = length . filter isValidRowB
+
+isValidRowA :: Row -> Bool
+isValidRowA r = r ^. #x <= i && i <= (r ^. #y)
+  where
+    i = length $ filter (== r ^. #c) (r ^. #pw)
+
+isValidRowB :: Row -> Bool
+isValidRowB r = isLetterAt (r ^. #x) `xor` isLetterAt (r ^. #y)
+  where
+    isLetterAt :: Int -> Bool
+    isLetterAt i = charAt (i - 1) (r ^. #pw) == Just (r ^. #c)
+
+data Row = MkRow
+  { x :: Int,
+    y :: Int,
+    c :: Char,
+    pw :: String
+  }
+  deriving stock (Eq, Ord, Show, Generic)
+
+passP :: Parser Row
+passP =
+  MkRow
+    <$> (L.decimal <* char '-')
+    <*> (L.decimal <* sc)
+    <*> (lowerChar <* symbol ":")
+    <*> some lowerChar
